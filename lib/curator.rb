58d8301194108691819a3f49f2dbd40329b094f7
Nico <nicovigil1@gmail.com>
2018-10-31 10:22:57 -0600

    finishes methods for curator

--- lib/curator.rb
+++ lib/curator.rb
@@ -17,4 +17,37 @@ class Curator
   def add_artist(attributes)
     @artists << Artist.new(attributes)
   end
+
+  def find_artist_by_id(artist_id)
+    @artists.find do |artist|
+      artist.id == artist_id
+    end
+  end
+
+  def find_photographs_by_artist(artist)
+    @photographs.find_all do |photo|
+      photo.artist_id == artist.id
+    end
+  end
+
+  def artists_with_multiple_photographs
+    chronic_painters = []
+    @artists.each do |artist|
+      if find_photographs_by_artist(artist).length > 1
+        chronic_painters << artist
+      end
+    end
+    chronic_painters
+  end
+
+  def photographs_taken_by_artists_from(nation)
+    national_artists = @artists.find_all do |artist|
+      artist.country == nation
+    end
+    answer = []
+    national_artists.each do |artist|
+      answer << find_photographs_by_artist(artist)
+    end
+    answer.flatten
+  end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -17,6 +17,19 @@ class CuratorTest < Minitest::Test
       artist_id: "2",
       year: "1941"
     }
+    @photo_3 = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    }
+    @photo_4 = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    }
+
     @artist_1 = {
       id: "1",
       name: "Henri Cartier-Bresson",
@@ -31,6 +44,28 @@ class CuratorTest < Minitest::Test
       died: "1984",
       country: "United States"
     }
+    @artist_3 = {
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    }
+    @curator_2 = Curator.new
+    @curator_2.add_photograph(@photo_1)
+    @curator_2.add_photograph(@photo_2)
+    @curator_2.add_photograph(@photo_3)
+    @curator_2.add_photograph(@photo_4)
+    @curator_2.add_artist(@artist_1)
+    @curator_2.add_artist(@artist_2)
+    @curator_2.add_artist(@artist_3)
+    @Henri = @curator_2.artists[0]
+    @Ansel = @curator_2.artists[1]
+    @Diane = @curator_2.artists[2]
+    @photo_1_object = @curator_2.photographs[0]
+    @photo_2_object = @curator_2.photographs[1]
+    @photo_3_object = @curator_2.photographs[2]
+    @photo_4_object = @curator_2.photographs[3]
   end
 
   def test_it_exists
@@ -60,6 +95,25 @@ class CuratorTest < Minitest::Test
     assert_equal "Henri Cartier-Bresson", @curator.artists[0].name
     assert_equal "Ansel Adams", @curator.artists[1].name
   end
+
+  def test_it_can_find_artist_by_id
+    assert_equal @Ansel, @curator_2.find_artist_by_id("2")
+  end
+
+  def test_it_can_find_photographs_by_artist
+    assert_equal [@photo_3_object, @photo_4_object], @curator_2.find_photographs_by_artist(@Diane)
+  end
+
+  def test_it_can_find_artist_with_multiple_photos
+    assert_equal [@Diane], @curator_2.artists_with_multiple_photographs
+    assert_equal 1, @curator_2.artists_with_multiple_photographs.length
+  end
+
+  def test_it_can_find_photos_by_country_of_photographers_origin
+    assert_equal [@photo_2_object, @photo_3_object, @photo_4_object], @curator_2.photographs_taken_by_artists_from("United States")
+    assert_equal [],  @curator_2.photographs_taken_by_artists_from("Venezuela")
+  end
+
 
 
 end
